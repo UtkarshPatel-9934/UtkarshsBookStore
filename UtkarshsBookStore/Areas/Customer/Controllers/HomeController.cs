bfbbe570fb7732154578be99177577ebebc121a8
Utkarsh Patel <W0785541@myscc.ca>
2022-04-19 13:52:45 -0400

    Added the code to display the products on the Home page...

--- UtkarshsBookStore/Areas/Customer/Controllers/HomeController.cs
+++ UtkarshsBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-
+using UtkarshsBooks.DataAccess.Repository.IRepository;
+using UtkarshsBooks.Models;
+using UtkarshsBookStore.Models.ViewModels;
 
 namespace UtkarshsBookStore.Area.Customer.Controllers
 {
@@ -14,15 +16,21 @@ namespace UtkarshsBookStore.Area.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        //added
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            //added
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
@@ -33,7 +41,9 @@ namespace UtkarshsBookStore.Area.Customer.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new UtkarshsBookStore.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // return View(new UtkarshsBookStore.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // added 
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
